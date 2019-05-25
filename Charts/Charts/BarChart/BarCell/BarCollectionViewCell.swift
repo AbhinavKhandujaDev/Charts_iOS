013db65fbc1d2ff7a1e89ea6f470db99e322c4eb
Abhinav <abhikhanduja@gmail.com>
2019-05-25 19:47:20 +0530

    Separate BarChartView implemented

---
+++ Charts/Charts/BarChart/BarCell/BarCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BarCollectionViewCell.swift
+//  Charts
+//
+//  Created by abhinav khanduja on 25/05/19.
+//  Copyright © 2019 abhinav khanduja. All rights reserved.
+//
+
+import UIKit
+
+class BarCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Charts/Charts/BarChart/BarChartView.swift
@@ -0,0 +1,21 @@
+//
+//  BarChartView.swift
+//  Charts
+//
+//  Created by abhinav khanduja on 25/05/19.
+//  Copyright © 2019 abhinav khanduja. All rights reserved.
+//
+
+import UIKit
+
+class BarChartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
